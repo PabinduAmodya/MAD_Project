57e626c386a7335126aa38dfcb0b87bcfb22ebfe
vishmipiumika <140807570+vishmipiumika@users.noreply.github.com>
2025-05-28 06:53:26 +0530

    Update welcome.dart

--- flutter_service_app_frontend/lib/screens/welcome.dart
+++ flutter_service_app_frontend/lib/screens/welcome.dart
@@ -1,1 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_service_app/screens/login.dart';
+import 'package:flutter_service_app/screens/user_type.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          // Background Image with Enhanced Effect
+          ShaderMask(
+            shaderCallback: (rect) {
+              return LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [Colors.black, Colors.transparent],
+              ).createShader(Rect.fromLTRB(0, 0, rect.width, rect.height));
+            },
+            blendMode: BlendMode.dstIn,
+            child: Image.asset(
+              "assets/background.png",
+              fit: BoxFit.cover,
+            ),
+          ),
 
