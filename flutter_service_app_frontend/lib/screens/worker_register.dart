78bd575962ce13209c405e46e5b88c4a0f4d6f5d
vishmipiumika <140807570+vishmipiumika@users.noreply.github.com>
2025-05-28 15:43:29 +0530

    Update worker_register.dart

--- flutter_service_app_frontend/lib/screens/worker_register.dart
+++ flutter_service_app_frontend/lib/screens/worker_register.dart
@@ -1,1 +1,24 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class WorkerRegisterScreen extends StatefulWidget {
+  const WorkerRegisterScreen({super.key});
+
+  @override
+  _WorkerRegisterScreenState createState() => _WorkerRegisterScreenState();
+}
+
+class _WorkerRegisterScreenState extends State<WorkerRegisterScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController phoneNoController = TextEditingController();
+  final TextEditingController locationController = TextEditingController();
+  final TextEditingController yearsExperienceController = TextEditingController();
+
+  final List<String> workTypes = ["Plumber", "Electrician", "Carpenter", "Mechanic", "Painter", "Mason", "Welder", "Cleaner"];
+  String? selectedWorkType;
+  String? workTypeError;
 
