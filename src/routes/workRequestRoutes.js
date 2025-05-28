0a0624a471e759b6a418d77f86c2b0f5281b1b74
PabinduAmodya <132426759+PabinduAmodya@users.noreply.github.com>
2025-05-28 10:01:18 +0530

    Initialize work request routes with creation and user retrieval

--- src/routes/workRequestRoutes.js
+++ src/routes/workRequestRoutes.js
@@ -1,11 +1,7 @@
 import express from 'express';
 import { 
   createWorkRequest, 
-  getUserWorkRequests, 
-  getWorkerWorkRequests, 
-  getWorkRequestById, 
-  updateWorkRequestStatus, 
-  addWorkRequestMessage 
+  getUserWorkRequests
 } from '../controllers/workRequestController.js';
 
 const router = express.Router();
@@ -15,22 +11,5 @@ router.post('/', createWorkRequest);
 
 // Get all work requests for the logged-in user
 router.get('/user', getUserWorkRequests);
-
-
-// Get all work requests for a specific worker (worker ID in params)
-router.get('/worker/:workerId?', getWorkerWorkRequests);
-
-
-// Get a specific work request by ID
-router.get('/:requestId', getWorkRequestById);
-
-
-// Update the status of a work request
-router.patch('/:requestId/status', updateWorkRequestStatus);
-
-
-// Add a message to a work request
-router.post('/:requestId/messages', addWorkRequestMessage);
-
 
 export default router;
