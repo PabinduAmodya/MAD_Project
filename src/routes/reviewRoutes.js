15fe308a8cc4cb44de53769a0125f963985e6791
Dilakshi216 <140873069+Dilakshi216@users.noreply.github.com>
2025-05-29 12:11:36 +0530

    Update reviewRoutes.js

--- src/routes/reviewRoutes.js
+++ src/routes/reviewRoutes.js
@@ -1,1 +1,11 @@
+// routes/reviewRoutes.js
+import express from "express";
+import { addReview, getWorkerReviews } from "../controllers/reviewController.js";
+import { authenticateUser } from "../middlewear/authmiddlewear.js"; 
 
+const router = express.Router();
+
+// Add a review (protected)
+router.post("/:workerId", authenticateUser, addReview);
+
+export default router;
