b9b26cf33561d2e51e64011d9ecf3266ce322a6d
Hasini Kosala Weerasinghe <140725937+H-K-Weerasinghe@users.noreply.github.com>
2025-05-29 20:49:11 +0530

    Update userModel.js

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,7 +1,9 @@
 class User {
     constructor(name, email, password, phoneNo, role = 'user') {
       this.name = name;
-      
+       this.email = email;
+      this.password = password;
+      this.phoneNo=phoneNo;
       this.role = role;
       this.createdAt = new Date();
     }
