bc1d54aa6d0688bb864910511bd4312d8cdda603
Hasini Kosala Weerasinghe <140725937+H-K-Weerasinghe@users.noreply.github.com>
2025-05-29 21:30:05 +0530

    Finalize workerModel.js

--- src/models/workerModel.js
+++ src/models/workerModel.js
@@ -1,1 +1,13 @@
+import User from './userModel.js';
 
+class Worker extends User {
+  constructor(name, email, password, workType, location, yearsOfExperience, phoneNo) {
+    // Fix the parameter order to match User constructor (name, email, password, phoneNo, role)
+    super(name, email, password, phoneNo, 'worker'); 
+    this.workType = workType;
+    this.location = location;
+    this.yearsOfExperience = yearsOfExperience;
+  }
+}
+
+export default Worker;
