9ceb1e3f7bc7c8b866f4fc2344a75b783c8e0ae1
Dilakshi216 <140873069+Dilakshi216@users.noreply.github.com>
2025-05-29 21:00:29 +0530

    Create class

--- flutter_service_app_frontend/lib/screens/payment.dart
+++ flutter_service_app_frontend/lib/screens/payment.dart
@@ -1,1 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/services.dart';
 
+class PaymentPage extends StatefulWidget {
+  final String userToken;
+  final String requestId;
+  final String workerName;
+  final String requestTitle;
+
+  const PaymentPage({
+    Key? key,
+    required this.userToken,
+    required this.requestId,
+    required this.workerName,
+    required this.requestTitle,
+  }) : super(key: key);
+
+  @override
+  _PaymentPageState createState() => _PaymentPageState();
+}
