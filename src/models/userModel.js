c9b3944ecc1ed4ec6d5f4c1e95eede30832525c1
Hasini Kosala Weerasinghe <140725937+H-K-Weerasinghe@users.noreply.github.com>
2025-05-29 20:48:26 +0530

    Add User class

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,1 +1,10 @@
-
+class User {
+    constructor(name, email, password, phoneNo, role = 'user') {
+      this.name = name;
+      
+      this.role = role;
+      this.createdAt = new Date();
+    }
+  }
+  
+  export default User;
