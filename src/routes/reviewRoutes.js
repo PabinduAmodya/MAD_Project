05513ce7eb90fe5fc9d73f919a4e18169de4f5cd
Dilakshi216 <140873069+Dilakshi216@users.noreply.github.com>
2025-05-29 13:08:37 +0530

    Update reviewRoutes.js

--- src/routes/reviewRoutes.js
+++ src/routes/reviewRoutes.js
@@ -8,4 +8,7 @@ const router = express.Router();
 // Add a review (protected)
 router.post("/:workerId", authenticateUser, addReview);
 
+// Get all reviews for a worker - make this public or authenticated
+router.get("/:workerId", getWorkerReviews);
+
 export default router;
