217930e7cd283aa2392ee9b3c62acfc8b97b9dc7
Hasini Kosala Weerasinghe <140725937+H-K-Weerasinghe@users.noreply.github.com>
2025-05-29 21:13:40 +0530

    Finalize userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -5,7 +5,8 @@ import { loginUser } from '../controllers/userController.js';
 
 const router = express.Router();
 
-
+router.post('/register', registerUser);
+router.post('/login', loginUser);
 
 
 export default router;
