af9db351c4623b8a1f2abacde204f1439e49ef07
Hasini Kosala Weerasinghe <140725937+H-K-Weerasinghe@users.noreply.github.com>
2025-05-29 21:12:50 +0530

    Add auth API routes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,1 +1,11 @@
+import express from 'express';
+import { registerUser } from '../controllers/userController.js';
+import { loginUser } from '../controllers/userController.js';
 
+
+const router = express.Router();
+
+
+
+
+export default router;
