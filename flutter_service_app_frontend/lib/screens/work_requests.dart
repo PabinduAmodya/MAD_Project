a8c5e665b3a3e48ec4e63078a8852faeeb9e9104
Dilakshi216 <140873069+Dilakshi216@users.noreply.github.com>
2025-05-29 18:53:49 +0530

    Create class

--- flutter_service_app_frontend/lib/screens/work_requests.dart
+++ flutter_service_app_frontend/lib/screens/work_requests.dart
@@ -1,1 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+
+class WorkRequestsPage extends StatefulWidget {
+  final String userToken;
+
+  const WorkRequestsPage({super.key, required this.userToken});
+
+  @override
+  _WorkRequestsPageState createState() => _WorkRequestsPageState();
+}
 
